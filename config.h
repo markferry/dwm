a6f5f78995fd1d7f4aaf9d7ab4564a4403ac290d
Mark Ferry <marfer01@arm.com>
2013-09-24 15:34:07 +0100

    Assign correct tags to Gnome-panel and Skype

--- config.h
+++ config.h
@@ -23,8 +23,8 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            True,        -1 },
-	{ "Gnome-panel",  NULL,   NULL,       1 << 9,       True,       -1 },
-	{ "Skype",    NULL,       NULL,       1 << 1,       False,       -1 },
+	{ "Gnome-panel",  NULL,   NULL,       1 << 8,       True,       -1 },
+	{ "Skype",    NULL,       NULL,       1 << 0,       False,       -1 },
 //	{ "Firefox",  NULL,       NULL,       1 << 2,       False,       -1 },
 };
 
